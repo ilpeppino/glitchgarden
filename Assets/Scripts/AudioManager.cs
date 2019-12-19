76f03a4332b7ef1101e8d6fd92a796d622ef2837
ilpeppino <ilpeppino@gmail.com>
2019-12-19 21:27:14 +0100

    122 - added splash and start screens, not completed

---
+++ Assets/Scripts/AudioManager.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Awake()
+    {
+        StartMusic();
+    }
+
+    private void StartMusic()
+    {
+        if (FindObjectsOfType(GetType()).Length > 1)
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            DontDestroyOnLoad(gameObject);
+        }
+    }
+}
+
