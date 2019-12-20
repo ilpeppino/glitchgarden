f91d9530a2f8977458f89383fa4a0db56bb6f7d0
ilpeppino <ilpeppino@gmail.com>
2019-12-20 10:57:11 +0100

    122-123: scene management + scaling

---
+++ Assets/Scripts/LevelManager.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelManager : MonoBehaviour
+{
+    /* PRIVATE VARS */
+    int currentSceneIndex = 0;
+    int timeToWait        = 3; 
+
+
+    private void Start()
+    {
+        // With this assignment, the current index is automatically determined from the build settings
+        currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+
+        if (currentSceneIndex == 0)
+        {
+            StartCoroutine(LoadStartScreen());
+        }
+    }
+
+    private IEnumerator LoadStartScreen()
+    {
+        yield return new WaitForSeconds(timeToWait);
+        LoadNextScene();
+    }
+
+    /******************/
+    /* PUBLIC METHODS */
+    /******************/
+
+    public void LoadNextScene()
+    {
+        SceneManager.LoadScene(currentSceneIndex + 1);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
