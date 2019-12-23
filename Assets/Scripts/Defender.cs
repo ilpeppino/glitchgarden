b63f0fc29456a266a367991f5385cfa2631a49d1
ilpeppino <ilpeppino@gmail.com>
2019-12-23 15:21:33 +0100

    136 - instantiating and shooting weapon

---
+++ Assets/Scripts/Defender.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Defender : MonoBehaviour
+{
+
+    [SerializeField] GameObject defenderWeapon, defenderGun;
+
+
+
+    public void Shoot()
+    {
+        Instantiate(defenderWeapon, defenderGun.transform.position, transform.rotation);
+        return;
+    }
+}
---
+++ Assets/Scripts/DefenderWeapon.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DefenderWeapon : MonoBehaviour
+{
+    private float weaponSpeed = 1f;
+
+    private void Update()
+    {
+        transform.Translate(Vector2.right * weaponSpeed * Time.deltaTime);
+
+    }
+}
