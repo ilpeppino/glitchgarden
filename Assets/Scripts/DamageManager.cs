e36893f2b6f10a1a1bf1e5c256b23cddc3a41550
ilpeppino <ilpeppino@gmail.com>
2019-12-25 12:29:15 +0100

    139 - destroying attacker and weapon when they collide and the attacker dies, adding explosion on attacker death

--- Assets/Scripts/DamageManager.cs
+++ Assets/Scripts/DamageManager.cs
@@ -1,11 +1,20 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+
+/************************************************
+ * USED BY
+ * 
+ * Prefabs/Lizard
+ * 
+ * **********************************************/
 
 public class DamageManager : MonoBehaviour
 {
 
     [SerializeField] float healthPoints = 100f;
+    [SerializeField] GameObject _explosion;
 
     public void Hit (float damagePoints)
     {
@@ -13,7 +22,18 @@ public class DamageManager : MonoBehaviour
 
         if (healthPoints <= 0)
         {
+            DeathExplosion();
             Destroy(gameObject);
         }
+    }
+
+    private void DeathExplosion()
+    {
+        if(!_explosion) { return; }
+
+        // In order to destroy the particles object, better to assign with the next statement and then destroy that object
+        GameObject _explosionObject = Instantiate(_explosion, transform.position, Quaternion.identity);
+        Destroy(_explosionObject, 2f);
+
     }
 }
--- Assets/Scripts/DefenderWeapon.cs
+++ Assets/Scripts/DefenderWeapon.cs
@@ -2,20 +2,37 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+/************************************************
+ * USED BY
+ * 
+ * Prefabs/Zucchini
+ * 
+ * **********************************************/
+
 public class DefenderWeapon : MonoBehaviour
 {
     [SerializeField] float damagePoints = 50f;
-    private float weaponSpeed = 1f;
+    [SerializeField] float weaponSpeed = 5f;
 
     private void Update()
     {
+        // The weapon goes always left to right from the defender's hands
         transform.Translate(Vector2.right * weaponSpeed * Time.deltaTime);
-
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        var _DamageManager = collision.GetComponent<DamageManager>();
-        _DamageManager.Hit(damagePoints);        
+        // When the defender weapon collides the attacker, it will inflict damage 
+        var _damageManager = collision.GetComponent<DamageManager>();
+        var _attacker = collision.GetComponent<Attacker>();
+
+        if (_attacker && _damageManager)
+        {
+            _damageManager.Hit(damagePoints);
+
+            // After the collision, the defender weapon gets destroyed
+            Destroy(gameObject);
+        }
+
     }
 }
